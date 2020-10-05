5f8d67464839c2508bf73f596a18b5d67a782ba1
Nandra Saputra <nandra.saputras@gmail.com>
2020-10-05 11:04:39 +0700

    improve viewpager height logic, add splash screen, improve detail UI (#3)

--- app/src/main/java/com/endiar/anyrecipes/ui/detail/overview/OverviewFragment.kt
+++ app/src/main/java/com/endiar/anyrecipes/ui/detail/overview/OverviewFragment.kt
@@ -1,21 +1,16 @@
 package com.endiar.anyrecipes.ui.detail.overview
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.endiar.anyrecipes.R
 import com.endiar.anyrecipes.adapter.IngredientAdapter
 import com.endiar.anyrecipes.core.data.Resource
-import com.endiar.anyrecipes.core.domain.model.RecipeFull
 import com.endiar.anyrecipes.ui.detail.DetailSharedViewModel
 import com.endiar.anyrecipes.utils.LinearItemDecoration
-import kotlinx.android.synthetic.main.fragment_detail.*
 import kotlinx.android.synthetic.main.fragment_overview.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
